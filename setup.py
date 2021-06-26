6c3e7ae32e9e8a946d7e50a4a96781163cda60a7
Breno-st <59622845+Breno-st@users.noreply.github.com>
2021-06-26 23:45:40 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,13 +12,3 @@ setup(name='ensemble',
       zip_safe=False)
 
 
-setup(name='optimization',
-      version='0.1',
-      description='Basic utilities for data science',
-      url='https://github.com/breno-st/ds-utils/src',
-      author='Breno Tiburcio',
-      author_email='b_tiburcio@hotmail.com',
-      license='MIT',
-      packages=find_packages(where="optimization"),
-      package_dir={"": "optimization"},
-      zip_safe=False)
