0e357bf72bbfb22874ad8349c905082ebbc77641
Breno-st <b_tiburcio@hotmail.com>
2021-06-26 23:22:40 +0200

    setup opt/ens

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import find_packages, setup
 
-setup(name='bootstrapped_aggregation',
+setup(name='ensemble',
       version='0.1',
       description='Basic utilities for data science',
-      url='https://github.com/breno-st/ds-utils/bootstrapped_aggregation',
-      author='Brendan Hasz',
+      url='https://github.com/breno-st/ds-utils/ensemble',
+      author='Breno Tiburcio',
       author_email='b_tiburcio@hotmail.com',
       license='MIT',
       packages=find_packages(where="src"),
@@ -12,11 +12,11 @@ setup(name='bootstrapped_aggregation',
       zip_safe=False)
 
 
-setup(name='ensemble',
+setup(name='optimization',
       version='0.1',
       description='Basic utilities for data science',
-      url='https://github.com/breno-st/ds-utils/ensemble',
-      author='Brendan Hasz',
+      url='https://github.com/breno-st/ds-utils/optimization',
+      author='Breno Tiburcio',
       author_email='b_tiburcio@hotmail.com',
       license='MIT',
       packages=find_packages(where="src"),
