94350c4eb5affe5fef104578b35e059d8d15fdf5
Breno-st <59622845+Breno-st@users.noreply.github.com>
2021-06-26 23:43:16 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,22 +3,22 @@ from setuptools import find_packages, setup
 setup(name='ensemble',
       version='0.1',
       description='Basic utilities for data science',
-      url='https://github.com/breno-st/ds-utils',
+      url='https://github.com/breno-st/ds-utils/src',
       author='Breno Tiburcio',
       author_email='b_tiburcio@hotmail.com',
       license='MIT',
-      packages=find_packages(where="src"),
-      package_dir={"": "src"},
+      packages=find_packages(where="ensemble"),
+      package_dir={"": "ensemble"},
       zip_safe=False)
 
 
 setup(name='optimization',
       version='0.1',
       description='Basic utilities for data science',
-      url='https://github.com/breno-st/ds-utils',
+      url='https://github.com/breno-st/ds-utils/src',
       author='Breno Tiburcio',
       author_email='b_tiburcio@hotmail.com',
       license='MIT',
-      packages=find_packages(where="src"),
-      package_dir={"": "src"},
+      packages=find_packages(where="optimization"),
+      package_dir={"": "optimization"},
       zip_safe=False)
