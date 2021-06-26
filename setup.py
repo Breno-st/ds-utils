09cbc14e265c6b760d9daadd6dce703a552bc584
Breno-st <59622845+Breno-st@users.noreply.github.com>
2021-06-26 19:27:56 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(name='dsutils',
+      version='0.1',
+      description='Basic utilities for data science',
+      url='https://github.com/breno-st/ds-utils',
+      author='Brendan Hasz',
+      author_email='b_tiburcio@hotmail.com',
+      license='MIT',
+      packages=find_packages(where="src"),
+      package_dir={"": "src"},
+      zip_safe=False)
