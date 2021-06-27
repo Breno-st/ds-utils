5652aada8bad562ac5adb5a4199a2371d512fc5b
Breno-st <b_tiburcio@hotmail.com>
2021-06-27 23:15:03 +0200

    setup

--- setup.py
+++ setup.py
@@ -1,14 +1,53 @@
-from setuptools import find_packages, setup
+import pathlib
+from setuptools import setup, find_packages
 
-setup(name='ensemble',
-      version='0.1',
-      description='Basic utilities for data science',
-      url='https://github.com/breno-st/ds-utils',
-      author='Breno Tiburcio',
-      author_email='b_tiburcio@hotmail.com',
-      license='MIT',
-      packages=find_packages(where="src"),
-      package_dir={"": "src"},
-      zip_safe=False)
+HERE = pathlib.Path(__file__).parent
+
+PACKAGE_NAME = 'ds-utils'
+AUTHOR = 'Breno Tiburcio'
+AUTHOR_EMAIL = 'b_tiburcio@hotmail.com'
+URL = 'https://github.com/breno-st/ds-utils'
+DOWNLOAD_URL = 'https://github.com/breno-st/ds-utils'
 
 
+
+LICENSE = 'MIT'
+VERSION = (HERE / "VERSION").read_text()
+DESCRIPTION = 'A set of data tools in Python'
+LONG_DESCRIPTION = (HERE / "DESCRIPTION.md").read_text()
+LONG_DESC_TYPE = "text/markdown"
+
+
+INSTALL_REQUIRES = ['numpy'
+      , 'pandas>=0.23.4'
+      , 'itertools'
+      , 'scipy'
+      , 'matplotlib'
+      , 'scikit-learn'
+      , 'scikit-plot>=0.3.7'
+      , 'itertools'
+      , 'collections'
+]
+
+CLASSIFIERS = [
+      'Programming Language :: Python :: 3'
+]
+
+PYTHON_REQUIRES = '>=3.5'
+
+
+setup(name=PACKAGE_NAME,
+      version=VERSION,
+      description=DESCRIPTION,
+      long_description=LONG_DESCRIPTION,
+      long_description_content_type=LONG_DESC_TYPE,
+      author=AUTHOR,
+      license=LICENSE,
+      author_email=AUTHOR_EMAIL,
+      url=URL,
+      download_url=DOWNLOAD_URL,
+      python_requires=PYTHON_REQUIRES,
+      install_requires=INSTALL_REQUIRES,
+      packages=find_packages(),
+      classifiers=CLASSIFIERS
+      )
